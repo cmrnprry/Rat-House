9764f38f5c9b2912e5426ca27243a0b3146aaa66
cmrnprry <perry.ca@husky.neu.edu>
2020-08-13 16:21:34 -0400

    Update CombatController.cs

--- Rat-House-Game/Assets/Scripts/Combat Scripts/CombatController.cs
+++ Rat-House-Game/Assets/Scripts/Combat Scripts/CombatController.cs
@@ -641,6 +641,7 @@ public class CombatController : MonoBehaviour
                 Debug.Log("Turn Over");
 
                 splashScreen[splashScreen.Length - 1].gameObject.SetActive(true);
+               
 
                 yield return new WaitForSecondsRealtime(1f);
 
